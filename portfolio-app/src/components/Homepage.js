6b84d1daef709c864442a91edefb1c26a01c7a3c
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-08-21 23:18:42 +0100

    React App and Homepage Components Created

---
+++ portfolio-app/src/components/Homepage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+const Homepage = () => {
+
+
+    return (
+        <section>
+            <header>
+
+            </header>
+            <section id="about">
+
+            </section>
+            <section id="offer">
+
+            </section>
+            <section id="tech">
+
+            </section>
+            <section id="portfolio">
+
+            </section>
+            <section id="contact">
+
+            </section>
+            <footer>
+
+            </footer>
+        </section>
+    )
+}
+
+export default Homepage
