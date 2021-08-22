5c71981f78ccaa7f3c1ce69710426d8a1a040486
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-08-22 02:22:43 +0100

    Portfolio Section Fixed with Styling

--- portfolio-app/src/components/Homepage.js
+++ portfolio-app/src/components/Homepage.js
@@ -35,29 +35,79 @@ const Homepage = () => {
             <section id="about">
                 <h2>About Me</h2>
                 <h5>
-                I am a Web Developer trained at Lambda School with years of experience in Finance, Accounting, Financial Modelling, Financial Reporting and Treasury Management. My passion to create, innovate and solve problems made me developed interest in Web Development. Before joining Lambda School, I have worked in the financial services industry as a Financial Controller where I and my team managed the financials and cashflows of the bank efficiently and profitably.
+                    I am a Web Developer trained at Lambda School with years of experience in Finance, Accounting, Financial Modelling, Financial Reporting and Treasury Management. My passion to create, innovate and solve problems made me developed interest in Web Development. Before joining Lambda School, I have worked in the financial services industry as a Financial Controller where I and my team managed the financials and cashflows of the bank efficiently and profitably.
                 </h5>
             </section>
             <section id="offer">
 
             </section>
             <section id="skills">
-                <img src={html} alt=""/>
-                <img src={css} alt=""/>
-                <img src={javascript} alt=""/>
-                <img src={bootstrap} alt=""/>
-                <img src={react} alt=""/>
-                <img src={redux2} alt=""/>
-                <img src={node} alt=""/>
-                <img src={postgresql} alt=""/>
-                <img src={express} alt=""/>
+                <img src={html} alt="" />
+                <img src={css} alt="" />
+                <img src={javascript} alt="" />
+                <img src={bootstrap} alt="" />
+                <img src={react} alt="" />
+                <img src={redux2} alt="" />
+                <img src={node} alt="" />
+                <img src={postgresql} alt="" />
+                <img src={express} alt="" />
             </section>
+            <h3>My Portfolios</h3>
             <section id="portfolio">
-                <img src={altrest_app} alt=""/>
-                <img src={ariya} alt=""/>
-                <img src={todoapp} alt=""/>
-                <img src={easybank} alt=""/>
-                <img src={altrest_web} alt=""/>  
+                <div className="container">
+                    <img src={altrest_app} alt="" />
+                    <div className="middle">
+                        <div className="text">
+                            <p>ALTREST is a tenant management platform </p>
+                            <p> React || Nodejs || Express || Bootstrap</p>
+                            <button>View</button></div>
+                    </div>
+                </div>
+                <div className="container">
+                    <img src={ariya} alt="" />
+                    <div className="middle">
+                        <div className="text">
+                            <p>ARIYA is an online marketplace, specifically designed for the event industry </p>
+                            <p> React || Nodejs || Express</p>
+                            <button>View</button></div>
+                    </div>
+                </div>
+                <div className="container">
+                    <img src={easybank} alt="" />
+                    <div className="middle">
+                        <div className="text">
+                            <p>EasyBank is Frontend Mentor Challenge </p>
+                            <p> HTML 5 ||CSS 3</p>
+                            <button>View</button></div>
+                    </div>
+                </div>
+                <div className="container">
+                    <img src={todoapp} alt="" />
+                    <div className="middle">
+                        <div className="text">
+                            <p>Todo App for activity management </p>
+                            <p> React || Nodejs || Express</p>
+                            <button>View</button></div>
+                    </div>
+                </div>
+                <div className="container">
+                    <img src={altrest_web} alt="" />
+                    <div className="middle">
+                        <div className="text">
+                            <p>ALTREST is a tenant management platform </p>
+                            <p> React || CSS 3 || Bootstrap</p>
+                            <button>View</button></div>
+                    </div>
+                </div>
+                <div className="container">
+                    <img src={easybank} alt="" />
+                    <div className="middle">
+                        <div className="text">
+                            <p>EasyBank is Frontend Mentor Challenge </p>
+                            <p> HTML 5 ||CSS 3</p>
+                            <button>View</button></div>
+                    </div>
+                </div>
             </section>
             <section id="contact">
 
